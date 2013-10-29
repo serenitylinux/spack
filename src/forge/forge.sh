5836c86e88a27768548057fc10ce0aa0c08b4f16
Christian Mesh <christianmesh1@gmail.com>
2013-10-28 21:31:41 -0400

    moved common functions to lib.sh

--- src/forge/forge.sh
+++ src/forge/forge.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ../lib.sh
+
 #Globals
 pretend=false
 run_test=false
@@ -9,67 +11,6 @@ MAKEFLAGS="-j4"
 tmp_dir="/tmp/forge/$$"
 src_dir="$tmp_dir/src"
 dest_dir="$tmp_dir/fs"
-
-# Usage: failexit critial_command args
-function failexit() {
-	$@
-	if [ $? -ne 0 ]; then 
-		log ERROR "$1 failed, exiting"
-		exit -1
-	fi
-}
-
-#Usage: color COLOR text
-function color() {
-	local color=$1
-	shift
-	local text=$@
-	local r_color
-	
-	case $color in
-		BLACK|black)
-			r_color="0;30";;
-		RED|red)
-			r_color="0;31";;
-		GREEN|green)
-			r_color="0;32";;
-		BROWN|brown)
-			r_color="1;33";;
-		BLUE|blue)
-			r_color="0;34";;
-		PURPLE|purple)
-			r_color="0;35";;
-		CYAN|cyan)
-			r_color="0;36";;
-		YELLOW|yellow)
-			r_color="1;33";;
-		WHITE|white)
-			r_color="1;37";;
-	esac
-	echo -en "\e[${r_color}m${text}\e[0m"
-}
-
-log_info=true
-log_debug=false
-log_warn=true
-log_error=true
-# TODO move to lib
-# Usage: log $level $string
-function log() {
-	local level="$1"
-	shift
-	local string="$@"
-	case $level in
-		INFO)
-			$log_info && echo $(color WHITE $@) ;;
-		DEBUG)
-			$log_debug && color BLUE "debug: " && echo $@ ;;
-		WARN)
-			$log_warn && color YELLOW "warning: " && echo $@ ;;
-		ERROR)
-			$log_error && color RED "error: " && echo $@ ;;
-	esac
-}
 
 #Default
 default_func="log ERROR Invalid Function; exit -1"
@@ -227,11 +168,13 @@ Usage: $0 [OPTIONS] package.pie
 	-h, --help
 
 EOT
-exit 1
+exit 0
 }
 
 function main() {
 	local option
+	local package
+	
 	for option in $@; do
 		case $option in
 			-p|--pretend)
@@ -249,7 +192,6 @@ function main() {
 				log_info=true
 				;;
 			-h|--help)
-				echo "Usage!"
 				usage;;
 			-t|--test)
 				run_test=true;;
---
+++ src/lib.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Usage: failexit critial_command args
+function failexit() {
+	$@
+	if [ $? -ne 0 ]; then 
+		log ERROR "$1 failed, exiting"
+		exit -1
+	fi
+}
+
+#Usage: color COLOR text
+function color() {
+	local color=$1
+	shift
+	local text=$@
+	local r_color
+	
+	case $color in
+		BLACK|black)
+			r_color="0;30";;
+		RED|red)
+			r_color="0;31";;
+		GREEN|green)
+			r_color="0;32";;
+		BROWN|brown)
+			r_color="1;33";;
+		BLUE|blue)
+			r_color="0;34";;
+		PURPLE|purple)
+			r_color="0;35";;
+		CYAN|cyan)
+			r_color="0;36";;
+		YELLOW|yellow)
+			r_color="1;33";;
+		WHITE|white)
+			r_color="1;37";;
+	esac
+	echo -en "\e[${r_color}m${text}\e[0m"
+}
+
+log_info=true
+log_debug=false
+log_warn=true
+log_error=true
+# TODO move to lib
+# Usage: log $level $string
+function log() {
+	local level="$1"
+	shift
+	local string="$@"
+	case $level in
+		INFO)
+			$log_info && echo $(color WHITE $@) ;;
+		DEBUG)
+			$log_debug && color BLUE "debug: " && echo $@ ;;
+		WARN)
+			$log_warn && color YELLOW "warning: " && echo $@ ;;
+		ERROR)
+			$log_error && color RED "error: " && echo $@ ;;
+	esac
+}
