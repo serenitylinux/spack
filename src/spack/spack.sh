056daa9c67a3e2d979ae6dfe6d4547a314cc6752
Christian Mesh <christianmesh1@gmail.com>
2013-12-03 11:10:27 -0500

    Spack: added refresh and upgrade option

--- src/spack/spack.sh
+++ src/spack/spack.sh
@@ -2,10 +2,18 @@
 
 source /usr/lib/spack/libspack
 
+function refresh_repos() {
+	for i in /etc/spack/repos/*; do
+		ls $i
+	done
+}
+
+
 function usage() {
 	cat <<EOT
 Usage: $0
-	check
+	refresh
+	upgrade
 	forge, build      [-f,--file]
 	wield, install    [-f,--file]
 	search
@@ -21,10 +29,13 @@ function main() {
 	local package
 	local file
 	case $1 in
-		check)
-			log ERROR "Check option not implemented"
+		refresh)
+			refresh_repos
 			exit -1
 			;;
+		upgrade)
+			log ERROR "upgrade option not implemented"
+			exit -1
 		-h|--help)
 			usage
 			exit 0
@@ -34,9 +45,12 @@ function main() {
 				-f|--file)
 					file="$3"
 				;;
+				*.pie)
+					file="$2"
+				;;
 				*)
 					package="$2"
-					file=$(get_package $package)
+					file=$(get_pie $package)
 				;;
 			esac
 			
@@ -47,12 +61,16 @@ function main() {
 		wield|install)
 			case $2 in
 				-f|--file)
-					wield $3
+					file="$3"
+				;;
+				*.spakg)
+					file="$2"
 				;;
 				*)
 					#search repo and get package[s]
-					package="$2"
+					#file=$(get_spakg $2)
 					log ERROR "installation from repo not supported"
+					exit 1
 				;;
 			esac
 			exit 0
