fb3b4c7c35168ac5fb691eab756e5598ec2f39c6
Christian Mesh <christianmesh1@gmail.com>
2014-07-12 10:21:47 -0400

    Break libspack/depres/pkgdep into several files

--- src/libspack/depres/pkgdep/pkgdep.go
+++ src/libspack/depres/pkgdep/pkgdep.go
@@ -2,41 +2,11 @@ package pkgdep
 
 import (
 	"fmt"
-	"libspack"
-	"libspack/log"
 	"libspack/dep"
-	"libspack/misc"
 	"libspack/repo"
 	"libspack/control"
 	"libspack/pkginfo"
-	"libspack/flagconfig"
 )
-/*****************************************
-Struct that represents how a parent depends on a child
-******************************************/
-type PkgDepParent struct {
-	Parent *PkgDep
-	dep dep.Dep
-	isbdep bool;
-}
-func (p * PkgDepParent) IsProudOf(pd *PkgDep) bool {
-	return p.dep.Flags.IsSubSet(pd.FlagStates)
-}
-
-type PkgDepParentList []PkgDepParent
-
-func (l *PkgDepParentList) Contains(p *PkgDep, isbdep bool) bool {
-	for _, n := range *l {
-		if p.Equals(n.Parent) && isbdep == n.isbdep {
-			return true
-		}
-	}
-	return false
-}
-
-func (l *PkgDepParentList) Append(p *PkgDep, deps dep.Dep, isbdep bool) {
-	*l = append(*l, PkgDepParent { p, deps, isbdep });
-}
 
 /******************************************
 Represents an installable package and it's rdeps
@@ -109,113 +79,4 @@ func (pd *PkgDep) IsInstalled(destdir string) bool {
 
 func (pd *PkgDep) Find(name string) *PkgDep {
 	return pd.Graph.Find(name)
-}
-
-/******************************************
-List of Pkg dependencies
-*******************************************/
-type PkgDepList []*PkgDep
-
-func (pdl *PkgDepList) Contains(pd *PkgDep) bool {
-	for _, item := range *pdl {
-		if item.Equals(pd) {
-			return true
-		}
-	}
-	return false
-}
-
-func (pdl *PkgDepList) Append(c *PkgDep) {
-	*pdl = append(*pdl, c)
-}
-//http://blog.golang.org/slices Magics
-func (pdl *PkgDepList) Prepend(c *PkgDep) {
-	*pdl = (*pdl)[0 : len(*pdl)+1] //Increase size by 1
-	copy((*pdl)[1:], (*pdl)[0:])   //shift array up by 1
-	(*pdl)[0] = c                  //set new first element
-}
-
-func (pdl *PkgDepList) Print() {
-	i := 0
-	for _, item := range *pdl {
-		str := item.String() + " "
-		i += len(str)
-		if i > misc.GetWidth()-10 {
-			fmt.Println()
-			i = len(str)
-		}
-		fmt.Print(str)
-	}
-	fmt.Println()
-}
-
-//http://stackoverflow.com/a/19239850
-func (pdl *PkgDepList) Reverse() {
-	for i, j := 0, len(*pdl)-1; i < j; i, j = i+1, j-1 {
-		(*pdl)[i], (*pdl)[j] = (*pdl)[j], (*pdl)[i]
-	}
-}
-func (pdl *PkgDepList) Add(dep string, destdir string) *PkgDep {
-	//Create new pkgdep node
-	ctrl, repo := libspack.GetPackageLatest(dep)
-	if ctrl == nil {
-		log.Error("Unable to find package ", dep)
-		return nil
-	}
-	
-	depnode := New(ctrl, repo)
-	pdl.Append(depnode)
-	
-	//Add global flags to new depnode
-	globalflags, exists := flagconfig.GetAll(destdir)[ctrl.Name]
-	if exists {
-		depnode.FlagStates = globalflags;
-	}
-	
-	//Find current rdeps and add them to the graph
-	
-	dep_info := repo.GetInstalledByName(dep, destdir)
-	if dep_info != nil {
-		rdeps := libspack.UninstallList(dep_info.PkgInfo)
-		for _, rdep := range rdeps {
-			current_info, repo := libspack.GetPackageInstalledByName(rdep.Control.Name, destdir)
-			parentnode := New(current_info.Control, repo)
-			
-			all_flags := current_info.PkgInfo.ComputedFlagStates()
-			all_deps := current_info.Control.ParsedDeps()
-			deps := all_deps.EnabledFromFlags(all_flags)
-			for _, d := range deps {
-				if d.Name == dep {
-					depnode.MakeParentProud(parentnode, d, false)
-					break;
-				}
-			}
-		}
-	}
-	
-	if !depnode.SatisfiesParents() { 
-		log.Error(dep, " unable to satisfy parents") //TODO more info
-//		return nil
-	}
-	
-	return depnode
-}
-func (pdl *PkgDepList) ToInstall(destdir string) *PkgDepList {
-	newl := make(PkgDepList, 0)
-	
-	for _, pkg := range *pdl {
-		if !pkg.ForgeOnly && !pkg.Repo.IsInstalled(pkg.PkgInfo(), destdir) {
-			newl.Append(pkg)
-		}
-	}
-	
-	return &newl
-}
-func (pdl *PkgDepList) Find(name string) *PkgDep {
-	for _, pkg := range *pdl {
-		if pkg.Control.Name == name {
-			return pkg
-		}
-	}
-	return nil
 }
